7af706186d4c199ada9e4bea7f1a9f96d11919b1
Bhautik Vaghasiya <bhautik.vaghasiya@webiots.com>
2023-06-29 09:02:17 +0530

    - templates convert to django

--- bigdealapp/urls.py
+++ bigdealapp/urls.py
@@ -5,4 +5,34 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('layout2',views.layout2,name='layout2'),
     path('layout3',views.layout3,name='layout3'),
+    path('layout4',views.layout4,name='layout4'),
+    path('megastore',views.megastore,name='megastore'),
+    path('layout5',views.layout5,name='layout5'),
+    path('layout6',views.layout6,name='layout6'),
+    path('furniture',views.furniture,name='furniture'),
+    path('cosmetic',views.cosmetic,name='cosmetic'),
+    path('kids',views.kids,name='kids'),
+    path('tools',views.tools,name='tools'),
+    path('grocery',views.grocery,name='grocery'),
+    path('pets',views.pets,name='pets'),
+    path('farming',views.farming,name='farming'),
+    path('digital_marketplace',views.digital_marketplace,name='digital_marketplace'),
+    
+    
+    
+    
+    
+# Shop pages routes
+    
+    path('shop_left_sidebar', views.shop_left_sidebar, name='shop_left_sidebar'),
+    path('shop_right_sidebar', views.shop_right_sidebar, name='shop_right_sidebar'),
+    path('shop_no_sidebar', views.shop_no_sidebar, name='shop_no_sidebar'),
+    path('shop_sidebar_popup', views.shop_sidebar_popup, name='shop_sidebar_popup'),
+    path('shop_metro', views.shop_metro, name='shop_metro'),
+    path('shop_full_width', views.shop_full_width, name='shop_full_width'),
+    path('shop_infinite_scroll', views.shop_infinite_scroll, name='shop_infinite_scroll'),
+    path('shop_3grid', views.shop_3grid, name='shop_3grid'),
+    path('shop_6grid', views.shop_6grid, name='shop_6grid'),
+    path('shop_list_view', views.shop_list_view, name='shop_list_view'),
+
 ]
--- bigdealapp/views.py
+++ bigdealapp/views.py
@@ -7,12 +7,98 @@ from django.http import HttpResponse, HttpRequest, HttpResponseBadRequest, HttpR
 # Create your views here.
 
 
+# HOME PAGES SECTION 
+
 def index(request):
     return render(request,'pages/home/ms1/index.html')
-
 
 def layout2(request):
     return render(request, 'pages/home/ms2/layout-2.html')
 
 def layout3(request):
     return render(request, 'pages/home/ms3/layout-3.html')
+
+def layout4(request):
+    return render(request, 'pages/home/ms4/layout-4.html')
+
+def megastore(request):
+    return render(request, 'pages/home/ms5/megastore.html')
+
+def layout5(request):
+    return render(request, 'pages/home/electronics/layout-5.html')
+
+def layout6(request):
+    return render(request, 'pages/home/vegetables/layout-6.html')
+
+def furniture(request):
+    return render(request, 'pages/home/furniture/furniture.html')
+
+def cosmetic(request):
+    return render(request,'pages/home/cosmetic/cosmetic.html')
+
+def kids(request):
+    return render(request, 'pages/home/kids/kids.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SHOP PAGES SECTION 
+
+def shop_left_sidebar(request):
+    return render(request, 'pages/shop/shop-left-sidebar.html')
+
+def shop_right_sidebar(request):
+    return render(request, 'pages/shop/shop-right-sidebar.html')
+
+def shop_no_sidebar(request):
+    return render(request, 'pages/shop/shop-no-sidebar.html')
+
+def shop_sidebar_popup(request):
+    return render(request, 'pages/shop/shop-sidebar-popup.html')
+
+def shop_metro(request):
+    return render(request, 'pages/shop/shop-metro.html')
+
+def shop_full_width(request):
+    return render(request, 'pages/shop/shop-full-width.html')
+
+def shop_infinite_scroll(request):
+    return render(request, 'pages/shop/shop-infinite-scroll.html')
+
+def shop_3grid(request):
+    return render(request, 'pages/shop/shop-3-grid.html')
+
+def shop_6grid(request):
+    return render(request, 'pages/shop/shop-6-grid.html')
+
+def shop_list_view(request):
+    return render(request, 'pages/shop/shop-list-view.html')
+
+def tools(request):
+    return render(request, 'pages/home/tools/tools.html')
+
+def grocery(request):
+    return render(request, 'pages/home/grocery/grocery.html')
+
+def pets(request):
+    return render(request, 'pages/home/pets/pets.html')
+
+def farming(request):
+    return render(request, 'pages/home/farming/farming.html')
+
+def digital_marketplace(request):
+    return render(request, 'pages/home/digital marketplace/digital-marketplace.html')
+
+
